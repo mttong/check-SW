2f36f7fda485db440a559fdc55b23b0e9eb5e578
sophipirie <pirie.s@northeastern.edu>
2025-02-17 13:51:05 -0500

    update camera code

--- test_frame.py
+++ test_frame.py
@@ -1,17 +1,19 @@
 from picamera2 import Picamera2
+#from libcamera import controls
 import cv2
 import time
 import pupil_apriltags as apriltag
 #import apriltag
 
 camera = Picamera2()
+
 config = camera.create_still_configuration(raw={}, display=None)
 camera.configure(config)
 
 camera.start()
 time.sleep(2)
-camera.capture_file("/home/check/Documents/image.jpg")
-img = cv2.imread("/home/check/Documents/image.jpg")
+camera.capture_file("/home/chess/Documents/check-SW/image.jpg")
+img = cv2.imread("/home/chess/Documents/check-SW/image.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
