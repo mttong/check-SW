db361fa8afc5505c40dde3373936a67d865d903d
sophipirie <pirie.s@northeastern.edu>
2025-03-29 17:21:34 -0400

    finish code for finding the left boundary of the chess board

--- test_frame.py
+++ test_frame.py
@@ -7,6 +7,14 @@ import numpy as np
 #import apriltag
 
 
+CHESS_PIECES = {
+	'black_pawn': 2,
+	'black_rook': 4,
+	'black_bishop': 6,
+	'black_knight': 8,
+	'black_queen': 10,
+	'black_king': 12
+}
 
 def take_image(camera):
 	time.sleep(0.5)
@@ -21,12 +29,37 @@ def detect_apriltags(img, brightness = 0.85):
 
 	detector = apriltag.Detector(families="tag36h11")
 	results = detector.detect(gray)
-
-	#print(f"Number of April Tags: {num_tags}")
-
-	#for tag in results:
-	#	print(f"April Tag ID: {tag.tag_id}, Center: {tag.center}")	
 	return results
+	
+def calibrate_board_left(results):
+	left_boundary_x = np.array([])
+	left_boundary_y = np.array([])
+	for tag in results:
+		#check if tag is black
+		if tag.tag_id % 2 == 0 and tag.tag_id != 2:
+			min_corner_x = 2048.0
+			min_corner = []
+			for corner in tag.corners:
+				if corner[0] < min_corner_x:
+					min_corner_x = corner[0]
+					min_corner = corner
+			left_boundary_x = np.append(left_boundary_x, min_corner_x)
+			left_boundary_y = np.append(left_boundary_y, min_corner[1])
+			print(f"Tag: {tag.tag_id}, Left Most Corner: {min_corner}")
+			
+	m, b = np.polyfit(left_boundary_x,left_boundary_y,1)
+	
+	img = cv2.imread("/home/chess/Documents/check-SW/image.jpg")
+	num_pixels_y = img.shape[1]
+	
+	x1, y1 = 0, int(b)
+	x2, y2 = num_pixels_y, int(m * num_pixels_y + b)
+	cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0) ,5)
+	for i in range(len(left_boundary_x)):
+		cv2.circle(img, (int(left_boundary_x[i]), int(left_boundary_y[i])), 10, (0, 0, 255), -1)
+	
+	cv2.imwrite("/home/chess/Documents/check-SW/image.jpg", img)
+	
 	
 if __name__ == "__main__":
 	camera = Picamera2()
@@ -40,7 +73,7 @@ if __name__ == "__main__":
 	
 	final_apriltag_results = []
 	num_tags = 0
-	for i in range(5):
+	for i in range(1):
 		img = take_image(camera)
 		results = detect_apriltags(img)
 		print(len(results))
@@ -50,6 +83,8 @@ if __name__ == "__main__":
 			
 	print(f"Number Tags: {num_tags}")
 	for tag in final_apriltag_results:
-		print(f"April Tag ID: {tag.tag_id}, Center: {tag.center}")	
+		print(f"April Tag ID: {tag.tag_id}, Center: {tag.center}, Corners: {tag.corners}")	
+	
+	calibrate_board_left(final_apriltag_results)
 	
 	camera.close()
